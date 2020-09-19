40d03ea3425a87e0bda97a3d3c36ffc18e60e399
Temirov Aziz <aziz9797@list.ru>
2020-09-19 15:02:34 +0500

    1.0 version of lib

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyMath.kt
@@ -0,0 +1,10 @@
+package com.example.mylibrary
+
+class MyMath {
+    companion object {
+        fun addition(num1: Int, num2: Int): Int = num1 + num2
+        fun subtraction(num1: Int, num2: Int): Int = num1 - num2
+        fun divide(num1: Int, num2: Int): Int = num1 / num2
+        fun mul(num1: Int, num2: Int): Int = num1 * num2
+    }
+}
